2385715e1b11865d7c838d06a6d41232a96ac0be
Ben Esse <benjamin.esse@manchester.ac.uk>
2021-11-26 15:20:50 +0000

    Update GPS module to also read date information

--- ifit/gps.py
+++ ifit/gps.py
@@ -1,4 +1,5 @@
 """Provides an interface to a USB GPS device."""
+import utm
 import time
 import serial
 import logging
@@ -14,20 +15,25 @@ logger = logging.getLogger(__name__)
 class GPS():
     """GPS object."""
 
-    def __init__(self, comport, filename=None, baudrate=4800,
+    def __init__(self, comport=None, filename=None, baudrate=4800,
                  parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,
                  bytesize=serial.EIGHTBITS):
         """Initialize."""
+        if comport is None:
+            comport = serial.tools.list_ports.comports()[0].device
         self.serial_port = serial.Serial(comport, baudrate=baudrate,
                                          parity=parity, stopbits=stopbits,
                                          bytesize=bytesize)
 
         self.filename = filename
         self.timestamp = None
+        self.datestamp = None
         self.lat = np.nan
         self.lon = np.nan
         self.alt = np.nan
+        self.utm_coords = None
         self.running = True
+        self.acquired = False
 
         self.thread = Thread(target=self._updater, daemon=True)
         self.thread.start()
@@ -51,6 +57,9 @@ class GPS():
 
                 if 'GGA' in data[0]:
                     self._parse_gpgga(data)
+
+                if 'RMC' in data[0]:
+                    self._parse_gprmc(data)
 
             except UnicodeDecodeError:
                 time.sleep(1)
@@ -81,7 +90,7 @@ class GPS():
                     lon = -lon
                 self.lon = lon
 
-            # Unpak altitude
+            # Unpack altitude
             alt_str = data[9]
             if alt_str != '':
                 alt = float(alt_str)
@@ -91,11 +100,65 @@ class GPS():
                 if alt_unit == 'F':
                     alt = 0.3048 * alt
                 self.alt = alt
-        except ValueError:
-            logger.warn('Error parsing GPS string.')
+
+            # Convert to UTM units
+            try:
+                self.utm_coords = utm.from_latlon(lat, lon)
+            except UnboundLocalError:
+                pass
+
+            self.acquired = True
+
+        except ValueError as e:
+            logger.warn(f'Error parsing GPS string\n{e}')
+
+    def _parse_gprmc(self, data):
+        """Parse NMEA GPRMC string."""
+        try:
+            # Read timestamp
+            self.timestamp = datetime.strptime(data[1], '%H%M%S.%f').time()
+
+            # Read date stamp
+            self.datestamp = datetime.strptime(data[9], '%d%m%y').date()
+
+            # Read lat/lon info
+            lat_str = data[3]
+            lat_dir = data[4]
+            lon_str = data[5]
+            lon_dir = data[6]
+
+            # Convert to decimel degrees
+            if lat_str != '':
+                lat = float(lat_str[:2]) + float(lat_str[2:])/60
+                if lat_dir == 'S':
+                    lat = -lat
+                self.lat = lat
+            if lon_str != '':
+                lon = float(lon_str[:3]) + float(lon_str[3:])/60
+                if lon_dir == 'W':
+                    lon = -lon
+                self.lon = lon
+
+        except ValueError as e:
+            logger.warn(f'Error parsing GPS string\n{e}')
+
+    def get_position(self,  time_to_wait=60):
+        """Report current time and position."""
+        t0 = datetime.now()
+        while (datetime.now() - t0).total_seconds() < time_to_wait:
+
+            flags = [~np.isnan(self.lat), ~np.isnan(self.lon),
+                     self.timestamp is not None, self.datestamp is not None]
+
+            if np.array(flags).all():
+                return [datetime.combine(self.datestamp, self.timestamp),
+                        self.lat, self.lon, self.alt]
+
+        logger.warning(f'No GPS fix acquired after {time_to_wait} seconds')
 
     def close(self):
         """Close the connection."""
         self.running = False
         self.thread.join()
         self.serial_port.close()
+        logger.info('GPS serial connection closed')
