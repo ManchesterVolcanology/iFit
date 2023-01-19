0ac46078675ee4a1f7bd8415857032cb24a7ccb7
Ben Esse <ben_esse@outlook.com>
2023-01-19 11:35:45 +0000

    Fix cmap source

--- iFitUI.py
+++ iFitUI.py
@@ -656,7 +656,7 @@ class MainWindow(QMainWindow):
         self.map_ax.setAspectLocked(True)
 
         # Generate the colorbar
-        self.cmap = pg.colormap.get('viridis', source='matplotlib')
+        self.cmap = pg.colormap.get('viridis')
         im = pg.ImageItem()
         self.cbar = pg.ColorBarItem(values=(0, 100), colorMap=self.cmap,
                                     label='Fit Value')
